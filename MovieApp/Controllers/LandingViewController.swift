7af56c7ba3e149efb4c8a80e4858bdbdd2a8c7dc
Thuyen Truong <thuyen.truong@eastagile.com>
2019-02-15 14:05:55 +0700

    [f] Create landing page with SignIn & SignUp buttons

---
+++ MovieApp/Controllers/LandingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Macintosh on 2/15/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
