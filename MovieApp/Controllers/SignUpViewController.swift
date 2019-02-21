5c60c4cc3764c1c3997496e3112cdf9bd56b6358
Thuyen Truong <thuyen.truong@eastagile.com>
2019-02-21 21:19:44 +0700

    [f] Create Sign Up screen

---
+++ MovieApp/Controllers/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  MovieApp
+//
+//  Created by Macintosh on 2/15/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
