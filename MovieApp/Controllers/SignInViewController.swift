f795808f31f8d249fdee943e45dd195d584a3d36
Thuyen Truong <thuyen.truong@eastagile.com>
2019-02-24 23:34:28 +0700

    [f] Create Sign In screen

---
+++ MovieApp/Controllers/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  MovieApp
+//
+//  Created by Macintosh on 2/25/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+  // MARK: - Init
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
