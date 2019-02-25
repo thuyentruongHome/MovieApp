bf10b8007f4065c6c071fd385331d9ed0496c169
Thuyen Truong <thuyen.truong@eastagile.com>
2019-02-25 11:34:46 +0700

    [f] Create Forgot Password Screen

---
+++ MovieApp/Controllers/ForgotPasswordViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ForgotPasswordViewController.swift
+//  MovieApp
+//
+//  Created by Macintosh on 2/25/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+}
